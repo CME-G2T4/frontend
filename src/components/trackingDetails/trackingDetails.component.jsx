5fdf7cceb6fcc9a6c7d43d337818d7e92760e41b
Jason <70564653+BioxMech@users.noreply.github.com>
2022-03-24 01:44:39 +0800

    Minor Tracking Details update

--- src/components/trackingDetails/trackingDetails.component.jsx
+++ src/components/trackingDetails/trackingDetails.component.jsx
@@ -14,19 +14,19 @@ function TrackingDetails() {
       <Box className="trackingDetails">
         <Grid container spacing={2}>
           <Grid item xs={12} sm={6}>
-            <span className="trackingTitle">Customer Name</span>
+            <span className="trackingTitle">Customer Name:</span>
           </Grid>
           <Grid item xs={12} sm={6}>
             <span className="trackingDesc">Bob Dylan</span>
           </Grid>
           <Grid item xs={12} sm={6}>
-            <span className="trackingTitle">Order Status</span>
+            <span className="trackingTitle">Order Status:</span>
           </Grid>
           <Grid item xs={12} sm={6}>
             <span className="trackingDesc">Moving</span>
           </Grid>
           <Grid item xs={12} sm={6}>
-            <span className="trackingTitle">Estimated Delivery Date </span>
+            <span className="trackingTitle">Estimated Delivery Date:</span>
           </Grid>
           <Grid item xs={12} sm={6}>
             <span className="trackingDesc">04/04/2022</span>
