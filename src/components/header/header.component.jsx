3dc5dc2ddd1baa7fe6e5368a135aa46aa0ed1b88
petrastyr <petrastyr@gmail.com>
2022-03-25 16:27:04 +0800

    Minor updates

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -27,7 +27,7 @@ export default function Header() {
   //   var item = pathname.split("/")[2];
   // }
 
-  const [openTH, setOpenTH] = useState(true);
+  // const [openTH, setOpenTH] = useState(true);
 
   // For the icons
   const useStyles = makeStyles({
