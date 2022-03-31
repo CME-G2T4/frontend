401f639abea983647b1ce11be593250399a55262
petrastyr <petrastyr@gmail.com>
2022-03-31 11:05:38 +0800

    Updates to components and pages
    - Updated order display component to include pagination
    - Skeleton for upload excel

--- src/components/orderDisplay/orderDetails.component.jsx
+++ src/components/orderDisplay/orderDetails.component.jsx
@@ -1,12 +1,98 @@
 import React from 'react';
 import Table from '@mui/material/Table';
+import Box from '@mui/material/Box';
+import { useTheme } from '@mui/material/styles';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
+import TableFooter from '@mui/material/TableFooter';
 import TableRow from '@mui/material/TableRow';
+import PropTypes from 'prop-types';
+import TablePagination from '@mui/material/TablePagination';
+import IconButton from '@mui/material/IconButton';
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import LastPageIcon from '@mui/icons-material/LastPage';
+
+function TablePaginationActions(props) {
+    const theme = useTheme();
+    const { count, page, rowsPerPage, onPageChange } = props;
+
+    const handleFirstPageButtonClick = (event) => {
+        onPageChange(event, 0);
+    };
+
+    const handleBackButtonClick = (event) => {
+        onPageChange(event, page - 1);
+    };
+
+    const handleNextButtonClick = (event) => {
+        onPageChange(event, page + 1);
+    };
+
+    const handleLastPageButtonClick = (event) => {
+        onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+    };
+
+    return (
+        <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+            <IconButton
+                onClick={handleFirstPageButtonClick}
+                disabled={page === 0}
+                aria-label="first page"
+            >
+                {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+            </IconButton>
+            <IconButton
+                onClick={handleBackButtonClick}
+                disabled={page === 0}
+                aria-label="previous page"
+            >
+                {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+            </IconButton>
+            <IconButton
+                onClick={handleNextButtonClick}
+                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+                aria-label="next page"
+            >
+                {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+            </IconButton>
+            <IconButton
+                onClick={handleLastPageButtonClick}
+                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+                aria-label="last page"
+            >
+                {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+            </IconButton>
+        </Box>
+    );
+}
+
+TablePaginationActions.propTypes = {
+    count: PropTypes.number.isRequired,
+    onPageChange: PropTypes.func.isRequired,
+    page: PropTypes.number.isRequired,
+    rowsPerPage: PropTypes.number.isRequired,
+};
 
 const OrderDetails = (props) => {
+
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+    const emptyRows =
+        page > 0 ? Math.max(0, (1 + page) * rowsPerPage - props.data.length) : 0;
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
 
     return (
         <TableContainer >
@@ -25,7 +111,7 @@ const OrderDetails = (props) => {
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {props.data.map((row) => (
+                    {/* {props.data.map((row) => (
                         <TableRow
                             key={row.order_id}
                             sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
@@ -42,8 +128,55 @@ const OrderDetails = (props) => {
                             <TableCell align="right">{row.order_status}</TableCell>
                             <TableCell align="right">{row.delivery_date}</TableCell>
                         </TableRow>
+                    ))} */}
+                    {(rowsPerPage > 0
+                        ? props.data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                        : props.data
+                    ).map((row) => (
+                        <TableRow
+                            key={row.fulfilment_id}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell component="th" scope="row">
+                                {row.order_id}
+                            </TableCell>
+                            <TableCell align="right">{row.customer_name}</TableCell>
+                            <TableCell align="right">Email</TableCell>
+                            <TableCell align="right">{row.order_address}</TableCell>
+                            <TableCell align="right">{row.order_datetime}</TableCell>
+                            <TableCell align="right">{row.order_details}</TableCell>
+                            <TableCell align="right">{row.tracking_no}</TableCell>
+                            <TableCell align="right">{row.order_status}</TableCell>
+                            <TableCell align="right">{row.delivery_date}</TableCell>
+                        </TableRow>
                     ))}
+
+                    {emptyRows > 0 && (
+                        <TableRow style={{ height: 53 * emptyRows }}>
+                            <TableCell colSpan={6} />
+                        </TableRow>
+                    )}
                 </TableBody>
+                <TableFooter>
+                    <TableRow>
+                        <TablePagination
+                            rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                            colSpan={12}
+                            count={props.data.length}
+                            rowsPerPage={rowsPerPage}
+                            page={page}
+                            SelectProps={{
+                                inputProps: {
+                                    'aria-label': 'rows per page',
+                                },
+                                native: true,
+                            }}
+                            onPageChange={handleChangePage}
+                            onRowsPerPageChange={handleChangeRowsPerPage}
+                            ActionsComponent={TablePaginationActions}
+                        />
+                    </TableRow>
+                </TableFooter>
             </Table>
         </TableContainer>
     )
