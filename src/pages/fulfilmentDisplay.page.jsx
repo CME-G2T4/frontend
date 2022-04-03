df6bdb4edef24069b0c3729a561b0bb8a84e0f9b
petrastyr <petrastyr@gmail.com>
2022-04-03 17:00:27 +0800

    Fixed useEffect

--- src/pages/fulfilmentDisplay.page.jsx
+++ src/pages/fulfilmentDisplay.page.jsx
@@ -23,7 +23,7 @@ const FulfilmentDisplay = () => {
                 setLoading(false);
             })
 
-    })
+    },[])
 
     return (
         <>
--- src/pages/orderDisplay.page.jsx
+++ src/pages/orderDisplay.page.jsx
@@ -25,7 +25,7 @@ const OrderDisplay = () => {
                 console.log(error);
             })
 
-    })
+    },[])
 
     return (
         <>
